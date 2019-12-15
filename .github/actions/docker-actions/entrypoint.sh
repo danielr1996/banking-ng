c0511b8831054fb7ffcd040a2c98d86a9b409edb
Daniel Richter <danielrichter@posteo.de>
2019-12-15 22:57:00 +0100

    test
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- .github/actions/docker-actions/entrypoint.sh
+++ .github/actions/docker-actions/entrypoint.sh
@@ -7,5 +7,5 @@ echo $INPUT_IMAGETAG
 echo $INPUT_DOCKERUSER
 echo $INPUT_DOCKERPASSWORD
 echo $INPUT_DOCKERPASSWORD | docker login --username $INPUT_DOCKERUSER --password-stdin
-docker build -t $INPUT_IMAGENAME:$INPUT_IMAGETAG .
+docker build -t $INPUT_IMAGENAME:$(echo $INPUT_IMAGETAG | tr / -) .
 echo ::set-output name=time::$time
