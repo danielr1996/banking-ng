268af5077060ca7be637d9e02d635e3ab8f6ef66
Daniel Richter <danielrichter@posteo.de>
2019-12-15 23:15:22 +0100

    test

--- .github/actions/docker-action/entrypoint.sh
+++ .github/actions/docker-action/entrypoint.sh
@@ -7,7 +7,7 @@ echo $INPUT_IMAGETAG
 echo $INPUT_DOCKERUSER
 echo $INPUT_DOCKERPASSWORD
 echo $INPUT_DOCKERPASSWORD | docker login --username $INPUT_DOCKERUSER --password-stdin
-#docker build -t $INPUT_IMAGENAME:$(echo $INPUT_IMAGETAG | tr / -) .
+docker build -t $INPUT_IMAGENAME:$(echo $INPUT_IMAGETAG | tr / -) .
 echo $INPUT_IMAGENAME:$(echo $INPUT_IMAGETAG | tr / -)
 docker push $INPUT_IMAGENAME:$(echo $INPUT_IMAGETAG | tr / -)
 echo ::set-output name=time::$time
