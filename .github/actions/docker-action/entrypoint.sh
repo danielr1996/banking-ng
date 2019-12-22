f20683363f1093a2f0b5e3883294592214a4b829
Daniel Richter <danielrichter@posteo.de>
2019-12-22 21:04:45 +0100

    feat(ci): Deploy to Kubernetes
    * feat: initial ci setup with docker build
    
    * feat: test action
    
    * fix action
    
    * fix action
    
    * make entrypoint executable
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * fix
    
    * test

---
+++ .github/actions/docker-action/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+
+echo ########################
+echo # Github Docker Action #
+echo ########################
+echo ImageName: $INPUT_IMAGENAME
+echo ImageTag: $INPUT_IMAGETAG
+#IMAGETAG_SLUG=$(echo $INPUT_IMAGETAG | tr / -)
+#echo ImageTagSlug: $IMAGETAG_SLUG
+echo DockerUser: $INPUT_DOCKERUSER
+echo DockerPasword: $INPUT_DOCKERPASSWORD
+
+# Login
+echo $INPUT_DOCKERPASSWORD | docker login --username $INPUT_DOCKERUSER --password-stdin
+
+# Build
+docker build -t $INPUT_IMAGENAME:$INPUT_IMAGETAG .
+
+# Push
+docker push $INPUT_IMAGENAME:$INPUT_IMAGETAG
